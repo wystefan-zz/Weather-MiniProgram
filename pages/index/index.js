b4f2da0ec3d5345ae2ec31a319e457b40615d974
wystefan <910327256@qq.com>
2020-08-01 14:53:36 -0700

    use mini program api to get weather and temp

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,37 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const weatherMap = {
+  'sunny': 'sunny',
+  'cloudy': 'cloudy',
+  'overcast': 'overcast',
+  'lighttrain': 'light rain',
+  'heavyrain': 'heavy rain',
+  'snow': 'snow'
+}
 
-Page({})
+Page({
+  data: {
+    nowTemp: '14',
+    nowWeather: 'cloundy'
+  },
+  onLoad() {
+    let that = this;
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: 'newyork'
+      },
+      success: function (res) {
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        console.log(temp, weather);
+        that.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather]
+        });
+      }
+    })
+  }
+})
